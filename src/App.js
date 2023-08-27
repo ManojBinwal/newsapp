7c00f7453a265c13ed39fe4869e612c460c42cc0
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-28 04:49:03 +0530

    adding author and date/time

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ export default class App extends Component {
         <Navbar />
           <Routes>
           {/* provide unique key inside newscomponent to remount on changing categories */}
-          <Route exact path="/"  element=<News key="general"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="general" /> />
-            <Route exact path="/science"  element=<News key="science"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="science" /> />
-            <Route exact path="/business"  element=<News key="business"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="business" /> />
-            <Route exact path="/sports"  element=<News key="sports"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="sports" /> />
-            <Route exact path="/entertainment"  element=<News key="entertainment"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" categorexact y="entertainment" /> />
-            <Route exact path="/technology"  element=<News key="technology"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="technology" /> />
-            <Route exact path="/health"  element=<News key="health"  pageSize={5} country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="health" /> />
+          <Route exact path="/"  element=<News key="general"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="general" /> />
+            <Route exact path="/science"  element=<News key="science"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="science" /> />
+            <Route exact path="/business"  element=<News key="business"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="business" /> />
+            <Route exact path="/sports"  element=<News key="sports"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="sports" /> />
+            <Route exact path="/entertainment"  element=<News key="entertainment"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" categorexact y="entertainment" /> />
+            <Route exact path="/technology"  element=<News key="technology"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="technology" /> />
+            <Route exact path="/health"  element=<News key="health"  author="unknown" date="" pageSize={9} source="unknown" country="in" apiKey="6d026a2663364991846858f60eedf8fb" category="health" /> />
           </Routes>
 
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,15 +8,22 @@ export class NewsItem extends Component {
     }
 
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, date, author, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card" >
                     <img src={imageUrl ? imageUrl : "https://cdn.vox-cdn.com/thumbor/AzUxs8UmwIY2lOByn5LIX8geWjY=/0x0:2200x1650/1200x628/filters:focal(1100x825:1101x826)/cdn.vox-cdn.com/uploads/chorus_asset/file/24835037/PayPal_stablecoin.png"} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                        <h5 className="card-title">{title}  <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:"1" , left:"90%"}}>
+                            {source}
+                            <span class="visually-hidden">unread messages</span>
+                        </span> </h5>
                         <p className="card-text">{description}</p>
+                        <p class="card-text"><small class="text-body-secondary">{author ? author : "Unknown"}</small></p>
                         <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">{new Date(date).toGMTString()}</small>
                     </div>
                 </div>
             </div>
