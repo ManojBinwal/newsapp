72e755c93fea66b9c132ad3ce30844eca01e101a
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-27 21:40:47 +0530

    adding api to fetch news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     constructor() {
         super();
-        console.log("Hello I'm a constructor")
+        console.log("Hello I'm a news-item constructor")
     }
 
     render() {
@@ -12,11 +12,11 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <img src={imageUrl ? imageUrl : "https://cdn.vox-cdn.com/thumbor/AzUxs8UmwIY2lOByn5LIX8geWjY=/0x0:2200x1650/1200x628/filters:focal(1100x825:1101x826)/cdn.vox-cdn.com/uploads/chorus_asset/file/24835037/PayPal_stablecoin.png"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/news-Item" className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
