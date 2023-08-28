8e3d0e2a7173816e2543e0919fe9c8fd0fd3d30b
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-28 23:38:24 +0530

    masking api key

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
   c = "John"
-  apiKey = "3442dd4c2fd1462787da05c659994e13"
+  apiKey = process.env.REACT_APP_NEWS_API
   country = "in"
 
   state = {
