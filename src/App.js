8c86e73b1577ddf4cb56803aa3a5741a639d25ea
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-29 03:01:27 +0530

    api changes and fixing errors

--- src/App.js
+++ src/App.js
@@ -8,36 +8,36 @@ import { useState } from 'react';
 
 const App = () => {
   const c = "John"
-  const apiKey = "768c40d3513204982ab829cecf984b3b"
+  const apiKey = "117c67bbc452dd061c4f7d65ca8ce465"
   const country = "in"
 
-  const[progress, setProgress] = useState(0);
-  
-    return (
-      <div>
-        <BrowserRouter>
-          <LoadingBar
-            color='#f11946'
-            progress={progress}
-            height={4}
-          />
-          <Navbar />
-          <Routes>
-            {/* provide unique key inside newscomponent to remount on changing categories */}
-            <Route exact path="/" element=<News setProgress={setProgress} key="general" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="general" /> />
-            <Route exact path="/science" element=<News setProgress={setProgress} key="science" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="science" /> />
-            <Route exact path="/business" element=<News setProgress={setProgress} key="business" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="business" /> />
-            <Route exact path="/sports" element=<News setProgress={setProgress} key="sports" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="sports" /> />
-            <Route exact path="/entertainment" element=<News setProgress={setProgress} key="entertainment" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} categorexact y="entertainment" /> />
-            <Route exact path="/technology" element=<News setProgress={setProgress} key="technology" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="technology" /> />
-            <Route exact path="/health" element=<News setProgress={setProgress} key="health" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="health" /> />
-          </Routes>
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <LoadingBar
+          color='#f11946'
+          progress={progress}
+          height={4}
+        />
+        <Navbar />
+        <Routes>
+          {/* provide unique key inside newscomponent to remount on changing categories */}
+          <Route exact path="/" element=<News setProgress={setProgress} key="general" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="general" /> />
+          <Route exact path="/science" element=<News setProgress={setProgress} key="science" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="science" /> />
+          <Route exact path="/business" element=<News setProgress={setProgress} key="business" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="business" /> />
+          <Route exact path="/sports" element=<News setProgress={setProgress} key="sports" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="sports" /> />
+          <Route exact path="/entertainment" element=<News setProgress={setProgress} key="entertainment" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} categorexact y="entertainment" /> />
+          <Route exact path="/technology" element=<News setProgress={setProgress} key="technology" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="technology" /> />
+          <Route exact path="/health" element=<News setProgress={setProgress} key="health" author="unknown" date="" pageSize={9} source="unknown" country={country} apiKey={apiKey} category="health" /> />
+        </Routes>
 
 
-        </BrowserRouter>
+      </BrowserRouter>
 
-      </div>
-    )
-  }
-  export default App
+    </div>
+  )
+}
+export default App
 
